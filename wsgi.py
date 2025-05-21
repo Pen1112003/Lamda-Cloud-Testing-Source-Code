a5382c1f75abbf6c9fdbffccc4f80179f0839486
Nguyen Duc Tuan <tuanndse173318@fpt.edu.vn>
2025-05-22 01:52:50 +0700

    Fix: Update application structure for Azure Web App

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Add the app directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 from app.app import app
 
 if __name__ == '__main__':
